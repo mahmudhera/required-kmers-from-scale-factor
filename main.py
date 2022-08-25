af3c670feb88de8489000e7915375c5ab11945d5
Mahmudur <mahmudhera93@gmail.com>
2022-08-25 13:54:11 -0400

    fixed typo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import math
 Given k, scale_factor in (0, 1) and bias_threshold, returns the minimum number of
 kmers required to make sure the bias is larger than the provided threshold.
 Example: scale_factor = 0.1, threshold = 0.99, k = 21 will return 44. This means
-we need >= 44 kmers to make sure the resulting bias >= 0.99
+we need >= 44 kmers to make sure the resulting bias factor >= 0.99, and hence the bias is <= 0.01
 '''
 def required_num_kmers(scale_factor, k, bias_threshold=0.99999):
     num_kmers = math.ceil(math.log(1.0 - bias_threshold) / math.log(1.0 - scale_factor))
